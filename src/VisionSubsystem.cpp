db4600d9c01bfd9b32b322d4fef3dd821940148f
Draven Schilling <Dravenschilling@gmail.com>
2016-01-21 20:41:16 -0600

    Added calculations for Camera Angles
    based on values output by the camera's (which are not added yet).  Will
    be used for auto bolder pickup.

--- src/VisionSubsystem.cpp
+++ src/VisionSubsystem.cpp
@@ -12,13 +12,55 @@ std::string VisionSubsystem::name(void){
 void VisionSubsystem::robotInit(void){
 
 	robot.outLog.appendLog("VisionSubsystem: RobotInit Success");
+
+
+
 }
 void VisionSubsystem::teleopInit(void){
 	robot.outLog.appendLog("VisionSubsystem: TeleopInit Success");
 
+
+
 }
 
 void VisionSubsystem::teleop(void){
+
+	const double cameraFOV = 68.5;
+	const double cameraMountAngle = 10; // Angle center of camera makes with the chassis //
+	const double distBetweenCameras = 10; //In inches //
+	const int maxXRes = 1280;
+
+	int leftCameraXPoint; //need to be supplied this with vision //
+	int rightCameraXPoint; // need to be supplied this with vision //
+
+	double relativeLeftAngle = 0;
+	double relativeRightAngle = 0;
+	double rightAngleError = 0;
+	double leftAngleError = 0;
+	double precentLeftAngle = leftCameraXPoint/maxXRes;
+	double precentRightAngle = rightCameraXPoint/maxXRes;
+
+	SmartDashboard::PutNumber("Left Camera Angle", relativeLeftAngle);
+	SmartDashboard::PutNumber("Right Camera Angle", relativeRightAngle);
+	SmartDashboard::PutNumber("Left Camera Angle Error", leftAngleError);
+	SmartDashboard::PutNumber("Right Camera Angle Error", rightAngleError);
+
+	// Angle Calculations for each side camera //
+	if(precentLeftAngle <= .5){
+		relativeLeftAngle = ((.5*cameraFOV)-(precentLeftAngle*cameraFOV))+(cameraMountAngle);
+	}else{
+		relativeLeftAngle =	cameraMountAngle-((precentLeftAngle*cameraFOV)-(.5*cameraFOV));
+	}
+	if(precentRightAngle <= .5){
+		relativeRightAngle = cameraMountAngle-((.5*cameraFOV)-(precentRightAngle*cameraFOV));
+	}else{
+		relativeRightAngle = ((precentRightAngle*cameraFOV)-(.5*cameraFOV))+(cameraMountAngle);
+	}
+	// Determine Angle Error //
+	leftAngleError = relativeLeftAngle - cameraMountAngle; // Positive Error is away from chassis //
+	rightAngleError = relativeRightAngle - cameraMountAngle; // Negative Error is towards chassis //
+
+
 
 	}
 
